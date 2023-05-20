f21718ca406de2c8641321e0a23f84023716e15c
771-8bit <kazuki.fujita8823@gmail.com>
2023-05-20 17:33:11 +0900

    Update GPS_test.ino

--- GPS_test/GPS_test.ino
+++ GPS_test/GPS_test.ino
@@ -1,15 +1,15 @@
 void setup() {
  SerialUSB.begin(115200);
- Serial.begin(9600);
+ Serial1.begin(115200);
 }
 
 void loop() {
   if(SerialUSB.available()){
     char c = (char)SerialUSB.read();
-    Serial.write(c);
+    Serial1.write(c);
   }
-  if(Serial.available()){
-    char c = (char)Serial.read();
+  if(Serial1.available()){
+    char c = (char)Serial1.read();
     SerialUSB.write(c);
   }
 }
